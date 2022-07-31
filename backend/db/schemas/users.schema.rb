84712150b0e773a54cf220d58c381a7ea95c908d
tetsu <cda07c5117@gmail.com>
2022-07-31 16:03:41 +0900

    ridgepole導入

---
+++ backend/db/schemas/users.schema.rb
@@ -0,0 +1,8 @@
+create_table :users, force: :cascade do |t|
+  t.string  "name", null: false
+  t.string  "email", null: false
+  t.string  "password", null: false
+  t.string  "profile", null: false, default: ""
+  t.datetime "created_at", null: false
+  t.datetime "updated_at", null: false
+end
