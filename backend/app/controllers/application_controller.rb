c57c13b83824f2301ffa2dc714413086b940de3c
tetsu <cda07c5117@gmail.com>
2022-09-23 12:46:05 +0900

    RequestForgeryProtection追加

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
   include ActionController::Helpers
+  include ActionController::RequestForgeryProtection
   
   helper_method :current_user, :user_signed_in?
   before_action :login_required
-
-  protect_from_forgery with: :exception
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
