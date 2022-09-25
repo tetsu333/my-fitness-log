fce8cf1e770c035c12a77ebdd4a07181d81d0252
tetsu <cda07c5117@gmail.com>
2022-09-25 23:46:47 +0900

    cookie設定変更

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
 
   def current_user
     # @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    @current_user ||= User.find_by(id: cookies[:user_id]) if cookies[:user_id]
+    @current_user ||= User.find_by(id: cookies.encrypted[:user_id]) if cookies.encrypted[:user_id]
   end
 
   def user_signed_in?
