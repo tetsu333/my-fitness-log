e4082cb8761f47ebbcc79c5ae53f3304c00f67ae
tetsu <cda07c5117@gmail.com>
2022-09-25 14:28:51 +0900

    config.hosts設定

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -42,5 +42,7 @@ module Myapp
     config.middleware.use ActionDispatch::ContentSecurityPolicy::Middleware
 
     config.i18n.default_locale = :ja
+
+    config.hosts << "my-fitness-log.jp"
   end
 end
