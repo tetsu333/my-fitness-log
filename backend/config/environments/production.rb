1e4b0b4838455b25c6d33a787cb603eac1d509f3
tetsu <cda07c5117@gmail.com>
2022-09-25 14:51:36 +0900

    config.hosts設定

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -111,4 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_dispatch.cookies_same_site_protection = nil
+
+  config.hosts << "my-fitness-log.jp"
 end
