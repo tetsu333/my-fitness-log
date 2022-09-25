b4ba18dbf521f1418713ab76e8a2324685cf0fc6
tetsu <cda07c5117@gmail.com>
2022-09-25 22:52:42 +0900

    cookie設定変更

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # config.force_ssl = true
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
@@ -111,5 +111,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_dispatch.cookies_same_site_protection = :none
-  config.session_store(secure: true)
+  config.session_store(secure: Rails.env.production?)
+  config.hosts << "my-fitness-log.jp"
 end
