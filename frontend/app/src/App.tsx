ebdc08e0be68506dc9a0ab0fba580f49870016e3
tetsu <cda07c5117@gmail.com>
2022-10-05 00:01:41 +0900

    HashRouterに変更

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import { Router } from "./router/Router";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Router />
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
